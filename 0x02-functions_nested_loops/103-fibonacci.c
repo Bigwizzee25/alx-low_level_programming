db2ba68436aa7ee38d87433c70243470494447fe
Bigwizzee25 <wisdomnkobie@gmail.com>
2022-09-16 06:41:14 -0700

    15. Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: a program that finds and prints the sum of the even-valued terms,
- * under 4 million, followed by a new line.
+ * Description: sum even fibonacci numbers under 4 million,
+ * followed by a new line.
  * Return: 0
  */
 int main(void)
