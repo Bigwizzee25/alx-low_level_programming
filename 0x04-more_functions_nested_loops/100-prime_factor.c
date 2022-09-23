5358ce5d61e0d01c06e4a7916497bdb9662df76c
Bigwizzee25 <wisdomnkobie@gmail.com>
2022-09-23 16:11:09 -0700

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 #include <math.h>
 /**
  * main - prime numbers
- * Return: 0 Always
+ * Return: Always 0
  */
+
 int main(void)
 {
 	unsigned int long n = 612852475143, a = (int) sqrt(n);
