767f40c688141a7b2ef1c69968721447f05f27c8
Bigwizzee25 <wisdomnkobie@gmail.com>
2022-09-29 14:24:39 -0700

    Dreams feel real while we're in them

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,14 @@
 
 /**
  *  _strlen_recursion - returns the length of a string
- *  @s: pointer to return length
+ *  @s: pointer to return length of string
  *  Return: int
  */
 int _strlen_recursion(char *s)
 {
 	int n = 0;
 
-	if (s < '\0')
+	if (*s > '\0')
 	{
 		n += _strlen_recursion(s + 1) + 1;
 	}
