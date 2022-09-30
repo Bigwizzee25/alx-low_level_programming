30b52e3554b74cc438eefb0c931087dd6d176af4
Bigwizzee25 <wisdomnkobie@gmail.com>
2022-09-30 14:28:06 -0700

    First commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 /**
  * _strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest.
- * @*dest: String that will be appended
- * @*src: String to be concatenuated upon
+ * @dest: String that will be appended
+ * @src: String to be concatenuated upon
  * Return: returns pointer to dest
  */
 char *_strcat(char *dest, char *src)
