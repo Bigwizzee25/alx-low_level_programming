baea617020c7794f6f2719e8952a9581dac8f9de
Bigwizzee25 <wisdomnkobie@gmail.com>
2022-10-05 20:13:52 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 			if (tab[i] == 0)
 			{
 				/*Free everything if malloc fails*/
-				while (--)
+				while (i--)
 					free(tab[i]);
 				free(tab);
 				return (NULL);
