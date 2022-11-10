191992229fb78350959eec63e9ea93aeacb85b53
Bigwizzee25 <wisdomnkobie@gmail.com>
2022-11-10 15:08:48 -0800

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen - function that counts string length
+ * @str: string to be used
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (length != '\0')
+		length++;
+	return (length);
+}
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: content of the file
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, rite;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		rite = write(file, text_content, _strlen(text_content));
+		if (rite == -1)
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
+}
